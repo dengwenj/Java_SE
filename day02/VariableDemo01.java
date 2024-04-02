1234d2840752574b6caa90eb0e36a33959129b41
朴睦 <927561237@qq.com>
2024-04-02 21:49:59 +0800

    变量

---
+++ day02/VariableDemo01.java
@@ -0,0 +1,19 @@
+public class VariableDemo01 {
+  public static void main(String[] args) {
+    // 数据类型 变量名 = 数据值;
+    // 数据类型：为空间中存储的数据，加入类型（限制）
+    int num = 24;
+    System.out.println(num);
+
+    // 一条语句可以定义多个变量
+    int num1 = 30, num2 = 40, num3 = 50;
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(num3);
+
+    // 变量在使用之前一定要进行赋值
+    int d;
+    d = 100;
+    System.out.println(d);
+  }
+}
