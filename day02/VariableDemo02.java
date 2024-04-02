892eb94f3090491ec59ce2e84e4668fb92028a35
朴睦 <927561237@qq.com>
2024-04-02 23:28:05 +0800

    数据类型

---
+++ day02/VariableDemo02.java
@@ -0,0 +1,36 @@
+public class VariableDemo02 {
+  public static void main(String[] args) {
+    // 基本数据类型
+    // 整数 byte short int long
+    // 浮点数 float double
+    // 字符 char
+    // boolean true false
+    byte a = 10;
+    System.out.println(a);
+
+    short b = 20;
+    System.out.println(b);
+
+    int c = 30;
+    System.out.println(c);
+
+    // long 类型需要加 L 标识
+    long d = 40L;
+    System.out.println(d);
+
+    // float 类型需要加 F 标识
+    float e = 50.5F;
+    System.out.println(e);
+
+    double f = 60.6;
+    System.out.println(f);
+
+    char g = 'w';
+    System.out.println(g);
+
+    boolean h = true;
+    System.out.println(h);
+    boolean i = false;
+    System.out.println(i);
+  }
+}
