1ed2a46d47124f37edf4f2f7d59d6e58d2959b25
朴睦 <927561237@qq.com>
2024-04-03 22:07:36 +0800

    ide 使用

---
+++ day02ide/index.md
@@ -0,0 +1,5 @@
+## IDEA 项目结构介绍
+* project(项目) -> java_learn
+* module(模块) -> day02ide
+* package(包) -> 文件夹 pm/ww
+* class(类) -> HelloWorld
---
+++ day02ide/src/pm/ww/HelloWorld.java
@@ -0,0 +1,8 @@
+package pm.ww;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 看情况的
+        System.out.println("你好");
+    }
+}
