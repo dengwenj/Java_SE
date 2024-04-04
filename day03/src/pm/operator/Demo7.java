8ca34297616fcb32fe4a2d573e4c0325e0d50d6c
朴睦 <927561237@qq.com>
2024-04-04 14:13:52 +0800

    三元表达式

---
+++ day03/src/pm/operator/Demo7.java
@@ -0,0 +1,14 @@
+package pm.operator;
+
+public class Demo7 {
+    public static void main(String[] args) {
+        // 三元表达式
+        // 作用：可以进行判断，根据判断的结果得到不同的内容
+        // 格式：关系表达式 ? 表达式1 : 表达式2
+
+        int num1 = 10;
+        int num2 = 20;
+        int max = num1 > num2 ? num1 : num2;
+        System.out.println(max); // 20
+    }
+}
