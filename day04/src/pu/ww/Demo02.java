dfb30bc3b81ae1191c6d3db1f28df932e99426fc
朴睦 <927561237@qq.com>
2024-04-04 17:42:33 +0800

    for循环

---
+++ day04/src/pu/ww/Demo02.java
@@ -0,0 +1,13 @@
+package pu.ww;
+
+public class Demo02 {
+    public static void main(String[] args) {
+        // for 循环
+        /*for(初始化语句; 条件判断语句; 条件控制语句) {
+            循环体语句;
+        }*/
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("朴睦" + i);
+        }
+    }
+}
