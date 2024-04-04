5168fd49a2106ff86c7bd909907e0ba929348e60
朴睦 <927561237@qq.com>
2024-04-04 13:33:37 +0800

    逻辑运算符

---
+++ day03/src/pm/operator/Demo05.java
@@ -0,0 +1,22 @@
+package pm.operator;
+
+public class Demo05 {
+    public static void main(String[] args) {
+        // 逻辑运算符
+        System.out.println(false & true); // false
+        System.out.println(true & true); // true
+        System.out.println(true & false); // false
+
+        System.out.println(false | true); // true
+        System.out.println(true | false); // true
+        System.out.println(false | false); // false
+        System.out.println(true | true); // true
+
+        System.out.println(false ^ false); // false
+        System.out.println(true ^ true); // false
+        System.out.println(false ^ true); // true
+
+        System.out.println(!false); // true
+        System.out.println(!true); // false
+    }
+}
