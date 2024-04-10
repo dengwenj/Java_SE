13ca5a72102eb0c6b9fcbe2c81241dac7c08454e
朴睦 <927561237@qq.com>
2024-04-10 15:06:30 +0800

    查找用户是否存在

---
+++ arraylist/src/pm/ww/Demo05.java
@@ -0,0 +1,43 @@
+package pm.ww;
+
+import java.util.ArrayList;
+
+public class Demo05 {
+    public static void main(String[] args) {
+        // 添加用户对象并判断是否存在
+        // main 方法中定义一个集合，存入三个用户对象
+        // id, username, password
+        // 定义一个方法，根据 id 查找对应的用户信息，存在返回 true，反之
+        ArrayList<Username> list = new ArrayList<>();
+
+        list.add(new Username("001", "朴睦", "123456"));
+        list.add(new Username("002", "李雷", "987654"));
+        list.add(new Username("003", "韩梅梅", "123321"));
+
+        boolean isExist = usernameById("0021", list);
+        System.out.println(isExist);
+
+        int idx = getUserIdxById("0021", list);
+        System.out.println(idx);
+    }
+
+    public static boolean usernameById(String id, ArrayList<Username> list) {
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i).getId().equals(id)) {
+//                return true;
+//            }
+//        }
+//
+//        return false;
+        return getUserIdxById(id, list) >= 0;
+    }
+
+    public static int getUserIdxById(String id, ArrayList<Username> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ arraylist/src/pm/ww/Username.java
@@ -0,0 +1,41 @@
+package pm.ww;
+
+public class Username {
+    private String id;
+    private String name;
+    private String password;
+
+    public Username() {
+
+    }
+
+    public Username(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
