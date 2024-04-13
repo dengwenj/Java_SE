6e3c85e4d255eac4d02f27498f405136b9ed03c6
朴睦 <927561237@qq.com>
2024-04-13 16:39:50 +0800

    包

---
+++ package/src/vip/dengwj/ww/Test.java
@@ -0,0 +1,11 @@
+package vip.dengwj.ww;
+
+import vip.dengwj.ww1.Person;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        Person p = new Person();
+    }
+}
