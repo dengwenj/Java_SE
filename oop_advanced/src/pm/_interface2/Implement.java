16b89970000a3e3007749604da8fad38235d5083
朴睦 <927561237@qq.com>
2024-04-13 22:19:34 +0800

    接口和类之间的关系

---
+++ oop_advanced/src/pm/_interface2/Implement.java
@@ -0,0 +1,33 @@
+package pm._interface2;
+
+public class Implement implements Inter3 {
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public void method2() {
+
+    }
+
+    @Override
+    public void method4() {
+
+    }
+
+    @Override
+    public void method5() {
+
+    }
+
+    @Override
+    public void method6() {
+
+    }
+
+    @Override
+    public void method3() {
+
+    }
+}
---
+++ oop_advanced/src/pm/_interface2/Inter2.java
@@ -0,0 +1,7 @@
+package pm._interface2;
+
+public interface Inter2 {
+    public abstract void method4();
+    public abstract void method5();
+    public abstract void method6();
+}
