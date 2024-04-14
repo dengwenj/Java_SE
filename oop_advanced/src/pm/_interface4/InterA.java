e21687a82ef13948e9e62ae1f2d5e34f13ff11d0
朴睦 <927561237@qq.com>
2024-04-14 14:31:43 +0800

    接口的静态方法和接口的私有方法

--- oop_advanced/src/pm/_interface4/InterA.java
+++ oop_advanced/src/pm/_interface4/InterA.java
@@ -5,5 +5,26 @@ public interface InterA {
 
     public default void show() {
         System.out.println("我是接口中定义的默认方法");
+        show3();
+    }
+
+    public default void show2() {
+        System.out.println("show");
+        show3();
+    }
+
+    public static void st() {
+        System.out.println("我是借口中的静态方法");
+        show4();
+    }
+
+    // 接口私有方法
+    private void show3() {
+        System.out.println("分别在 show、show2 中调用");
+    }
+
+    // 静态私有方法
+    private static void show4() {
+        System.out.println("在静态中调用");
     }
 }
--- oop_advanced/src/pm/_interface4/Test.java
+++ oop_advanced/src/pm/_interface4/Test.java
@@ -5,5 +5,9 @@ public class Test {
         InterImpl ii = new InterImpl();
         ii.method();
         ii.show();
+
+        // 子类把从父类继承下来的虚方法表里面的方法进行覆盖了，这才叫重写
+        // 接口中的静态方法不能被重写
+        InterA.st();
     }
 }
