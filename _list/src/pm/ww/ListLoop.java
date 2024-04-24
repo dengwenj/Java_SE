4fda0e30e66a7910f273b3886ee418cfe7df2edf
朴睦 <927561237@qq.com>
2024-04-24 22:48:16 +0800

    LinkedList 集合

--- _list/src/pm/ww/ListLoop.java
+++ _list/src/pm/ww/ListLoop.java
@@ -1,9 +1,6 @@
 package pm.ww;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class ListLoop {
     public static void main(String[] args) {
