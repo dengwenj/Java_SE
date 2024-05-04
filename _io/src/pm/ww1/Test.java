e40f5601bec4d4d9ee46b83081b04532c2f77c8c
朴睦 <927561237@qq.com>
2024-05-04 16:00:30 +0800

    字节文件输出流FileOutputStream

---
+++ _io/src/pm/ww1/Test.java
@@ -0,0 +1,15 @@
+package pm.ww1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+        // FileOutputStream
+        FileOutputStream fos = new FileOutputStream("_io/test.txt");
+        // 输出到文件中
+        fos.write(97);
+        // 释放资源
+        fos.close();
+    }
+}
