d06ee85bd94d858590f017d560025b1923faa886
朴睦 <927561237@qq.com>
2024-05-05 22:27:36 +0800

    反序列化流/对象操作输入流

--- _io/index.md
+++ _io/index.md
@@ -253,3 +253,9 @@ public class Test {
 * public final void writeObject(Object obj); 把对象序列化（写出）到文件中去
 * 细节：使用对象输出流将对象保存到文件时会出现 NotSerializableException 异常
 * 解决方案：需要让 javabean 类实现 Serializable 接口
+
+## 反序列化流/对象操作输入流
+* 可以把序列化到本地文件中的对象，读取到程序中来
+* public ObjectInputStream(InputStream out); 把基本流变成高级流
+* public Object readObject(); 把序列化到本地文件中的对象，读取到程序中来
+
---
+++ _io/src/pm/objectstream/Test2.java
@@ -0,0 +1,16 @@
+package pm.objectstream;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class Test2 {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("_io/src/pm/objectstream/test.txt"));
+
+        Student o = (Student) ois.readObject();
+        System.out.println(o); // Student{name = 朴睦, age = 24}
+
+        ois.close();
+    }
+}
