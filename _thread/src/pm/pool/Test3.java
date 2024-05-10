86f4971d2286e42005e0e90992b95682032a58ff
朴睦 <927561237@qq.com>
2024-05-10 17:47:04 +0800

    多线程额外知识

---
+++ _thread/src/pm/pool/Test3.java
@@ -0,0 +1,8 @@
+package pm.pool;
+
+public class Test3 {
+    public static void main(String[] args) {
+        // 向 java 虚拟机返回可用处理器的数目
+        System.out.println(Runtime.getRuntime().availableProcessors()); // 8
+    }
+}
