95f5b019ecc54a622497c4be1b8133b126503e9e
朴睦 <927561237@qq.com>
2024-05-11 14:53:39 +0800

    TCP 上传文件用线程池

--- _network/src/pm/anli/demo05/Server.java
+++ _network/src/pm/anli/demo05/Server.java
@@ -3,16 +3,32 @@ package pm.anli.demo05;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(1209);
 
+        // 用线程池
+        ThreadPoolExecutor pools = new ThreadPoolExecutor(
+          3,
+          16,
+          60,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(2),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.AbortPolicy()
+        );
+
         while (true) {
             // 等待客户端连接
             Socket accept = ss.accept();
 
-            new Thread(new MyRunnable(accept)).start();
+            // new Thread(new MyRunnable(accept)).start();
+            pools.submit(new MyRunnable(accept));
         }
     }
 }
