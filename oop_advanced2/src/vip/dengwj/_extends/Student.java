e95b660716173d93be68a63504515727ac08a2c7
朴睦 <927561237@qq.com>
2024-06-20 14:34:18 +0800

    继承

---
+++ oop_advanced2/src/vip/dengwj/_extends/Student.java
@@ -0,0 +1,7 @@
+package vip.dengwj._extends;
+
+public class Student extends Person {
+    public void study() {
+        System.out.println("学习");
+    }
+}
---
+++ oop_advanced2/src/vip/dengwj/_extends/Test.java
@@ -0,0 +1,9 @@
+package vip.dengwj._extends;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.eat();
+        student.study();
+    }
+}
